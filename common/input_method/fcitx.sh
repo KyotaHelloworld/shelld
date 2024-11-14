6e7916eabb7ce2352ccf853d7d4202c10a9e74a6
kyota <kyota.helloworld@gmail.com>
2024-11-14 15:47:37 +0000

    update fcitx

--- common/input_method/fcitx.sh
+++ common/input_method/fcitx.sh
@@ -2,14 +2,15 @@
 function input_method_fcitx() {
     local this_dir=$(dirname $(type ${BASH_SOURCE:-$0} | awk '{print $NF}'))
 
-    if [[ -z $GTK_IM_MODULE ]] || [[ -z $XMODIFIERS ]] || [[ -z $QT_IM_MODULE ]]; then
+    # if [[ -z $GTK_IM_MODULE ]] || [[ -z $XMODIFIERS ]] || [[ -z $QT_IM_MODULE ]]; then
+    if [[ -z $XMODIFIERS ]]; then
         echo "[SUGGESTION] put environment for fcitx in /etc/environment"
         echo "[SUGGESTION] check exporting env value in $this_dir"
     fi
 
-    export GTK_IM_MODULE=${GTK_IM_MODULE:-"fcitx"}
+    # export GTK_IM_MODULE=${GTK_IM_MODULE:-"fcitx"}
+    # export QT_IM_MODULE=${QT_IM_MODULE:-"fcitx"}
     export XMODIFIERS=${XMODIFIERS:-"@im=fcitx"}
-    export QT_IM_MODULE=${QT_IM_MODULE:-"fcitx"}
 }
 
 input_method_fcitx
